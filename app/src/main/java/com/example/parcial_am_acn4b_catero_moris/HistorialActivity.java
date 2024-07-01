78da7f9bc7c3a2616492031a13480186df81cd3d
dvsantiagocatero <santiago.catero@davinci.edu.ar>
2024-07-01 17:49:54 -0300

    Create HistorialActivity.java
    feat: Creacion HistorialActivity

---
+++ app/src/main/java/com/example/parcial_am_acn4b_catero_moris/HistorialActivity.java
@@ -0,0 +1,14 @@
+package com.example.parcial_am_acn4b_catero_moris;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
+public class HistorialActivity extends AppCompatActivity {
+}
