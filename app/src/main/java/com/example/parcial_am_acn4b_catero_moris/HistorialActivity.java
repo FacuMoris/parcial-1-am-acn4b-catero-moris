ebab4619aa435d971781b0c80535e4b880d71cc8
dvsantiagocatero <santiago.catero@davinci.edu.ar>
2024-07-01 23:14:11 -0300

    Update HistorialActivity.java
    feat: Eliminar Import

--- app/src/main/java/com/example/parcial_am_acn4b_catero_moris/HistorialActivity.java
+++ app/src/main/java/com/example/parcial_am_acn4b_catero_moris/HistorialActivity.java
@@ -8,7 +8,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.view.View;
 
